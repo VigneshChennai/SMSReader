06a62984032af391994da50efd21d3968a74fd49
Vigneshwaran P <vigneshchennai@live.in>
2015-02-11 15:44:53 +0530

    Fixed configuration read issue in smsreader.py

--- smsreader.py
+++ smsreader.py
@@ -17,16 +17,16 @@ class Color:
     BOLD = "\033[1m"
     UNDERLINE = '\033[4m'
     END = "\033[0m"
-    
+
 class SMS():
-    
+
     def __init__(self, subj, from_no, date, msg, udh = None):
         self.subj = subj
         self.from_no = from_no
         self.date = date
         self.msg = msg
         self.udh = udh
-    
+
     def __eq__(self, sms2):
         try:
             if self.subj != sms2.subj or self.from_no != sms2.from_no \
@@ -36,52 +36,52 @@ class SMS():
         except AttributeError:
             return False
         return True
-    
+
     def __repr__(self):
         return "Subj : %s, From: %s, Date: %s" % (
                 self.subj, self.from_no, self.date.strftime('%Y-%m-%d %H:%M:%S'))
 
 
 class CSMS():
-    
+
     def __init__(self, parts_count):
         self.parts_count = parts_count
         self.parts = {}
-    
+
     def addpart(self, sms):
         partid = sms.udh['PartNumber']
         self.parts[partid] = sms
-        
+
     def getfullsms(self):
         if self.parts_count == len(self.parts):
             smsparts = [None] * self.parts_count
             for key, value in self.parts.items():
                 smsparts[key - 1] = value
-                
+
             msgparts = []
             for i in smsparts:
                 msgparts.append(i.msg)
-            sms = SMS(smsparts[0].subj, smsparts[0].from_no, smsparts[-1].date, 
+            sms = SMS(smsparts[0].subj, smsparts[0].from_no, smsparts[-1].date,
                       "".join(msgparts))
             return sms
         else:
-            return None 
-            
+            return None
+
 class CSMSManager():
-    
+
     def __init__(self):
         self.csmsmap = {}
-    
+
     def addCSMSpart(self, sms):
         id = sms.udh['ID16bit'] if sms.udh['ID16bit'] != -1 else sms.udh['ID8bit']
-        
+
         try:
             self.csmsmap[id].addpart(sms)
         except KeyError:
             csms = CSMS(sms.udh['AllParts'])
             csms.addpart(sms)
-            self.csmsmap[id] = csms 
-                                   
+            self.csmsmap[id] = csms
+
     def getreadyCSMS(self):
         smses = []
         dels = []
@@ -92,36 +92,36 @@ class CSMSManager():
                smses.append(sms)
         for d in dels:
             del self.csmsmap[d]
-        return smses 
-                
+        return smses
+
 
 class SMSReader():
-    
+
     def __init__(self):
         sms_folder = os.getcwd()
         if not os.access(sms_folder + '/inbox', os.F_OK):
             os.mkdir(sms_folder + '/inbox')
-        
+
         self.sms_folder = sms_folder
         self.sm = gammu.StateMachine()
-        self.sm.ReadConfig(sms_folder + "/gammurc")
-        
+        self.sm.ReadConfig(Filename = sms_folder + "/gammurc")
+
         self.csmsmanager = CSMSManager()
-        
+
         self._lastreadsms = self.lastreadsms()
         self.location_flag = 0
         self.start_flag = 1
-    
-    def lastreadsms(self, sms=None):
+
+    def lastreadsms(self, sms=None):    
         if sms:
             with open(self.sms_folder + "/lastmsg.pickle", "w") as f:
                 pickle.dump(sms, f)
             self._lastreadsms = sms
         else:
             if os.access(self.sms_folder + "/lastmsg.pickle", os.F_OK):
-                with open(self.sms_folder + "/lastmsg.pickle") as f:    
+                with open(self.sms_folder + "/lastmsg.pickle") as f:
                     return pickle.load(f)
-    
+
     def smstofile(self, sms):
         filename = sms.from_no + "_" + sms.date.strftime("%Y-%m-%d_%H:%M:%S")
         ext = ".sms"
@@ -134,16 +134,16 @@ class SMSReader():
             else:
                 break
         fullfile = self.sms_folder + '/' + 'inbox/' + t + ext
-        print Color.BOLD + "Writing SMS to file: " + Color.END + str(sms) + Color.BOLD +  "\nFile location : " + Color.END + str(fullfile)  
+        print Color.BOLD + "Writing SMS to file: " + Color.END + str(sms) + Color.BOLD +  "\nFile location : " + Color.END + str(fullfile)
         with open(fullfile, "w") as f:
             f.write(sms.from_no)
             f.write("\n")
             f.write(sms.msg)
-        
+
     def start(self):
         self.connect()
-        try:          
-            while True:  
+        try:
+            while True:
                 smses = self.readinbox()
                 if len(smses) > 0:
                     self.lastreadsms(smses[0])
@@ -158,13 +158,13 @@ class SMSReader():
                 time.sleep(30)
         finally:
             self.disconnect()
-        
+
     def connect(self):
         self.sm.Init()
 
     def disconnect(self):
         self.sm.Terminate()
-    
+
     def readinbox(self):
         print "Reading Inbox .."
         smses = []
@@ -183,7 +183,7 @@ class SMSReader():
                       from_no = output[0]['Number'],
                       date = output[0]['DateTime'],
                       msg = output[0]['Text'],
-                      udh = output[0]['UDH'] if output[0]['UDH']["AllParts"] > 0 else None) 
+                      udh = output[0]['UDH'] if output[0]['UDH']["AllParts"] > 0 else None)
             print Color.BOLD + "Read : " + Color.END + repr(sms)
             if sms == self._lastreadsms:
                 print Color.BOLD + "SMS read is same as last read SMS. Skip further SMS reading in Inbox..." + Color.END
@@ -193,15 +193,15 @@ class SMSReader():
             else:
                 if self.start_flag == 1 and self.location_flag == 0:
                     self.start_flag = 0
-                    self.location_flag = 1    
+                    self.location_flag = 1
                 return sms
         except gammu.ERR_EMPTY:
             if self.start_flag == 0 and self.location_flag == 1:
                 self.start_flag = 1
                 self.location_flag = 0
             return None
-            
-                
+
+
 if __name__ == '__main__':
-    smsr = SMSReader('/content/SMS')
+    smsr = SMSReader()
     smsr.start()
